53c95f2ec2170f07865b783f68908e8a493fe246
student <student@lab-44-15.cs.put.poznan.pl>
2018-10-31 10:46:17 +0100

    Zmiana wyswietlanego tekstu

--- program.java
+++ program.java
@@ -1,5 +1,6 @@
 public class NewName{
   public static void main(String[] args){
-    System.out.print("Hello World 2345");
+    System.out.print("Hello World");
+	System.out.println("No siema");
   }
 }
